ab40ba963da9364fc23419c8039431379432c7af
Vladislav Knyshov <inova136@gmail.com>
2021-10-30 01:12:12 +0300

    update util functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
-export function hasItemWithTheSameId<T extends { id: string }>(
-  array: T[],
-  item: T
-) {
-  return array.find((element) => element.id === item.id);
+interface WithId {
+  id: string;
+}
+
+export function removeItemWithId<T extends WithId>(array: T[], item: T) {
+  return array.filter((element) => element.id !== item.id);
+}
+
+export function addIfNoCopies<T extends WithId>(array: T[], item: T) {
+  if (array.find((element) => element.id === item.id)) return array;
+  return [...array, item];
 }
