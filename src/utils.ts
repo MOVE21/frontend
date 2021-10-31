f78c52eafa5b93f334bb31d0d0936551f0173245
Vladislav Knyshov <inova136@gmail.com>
2021-10-31 19:25:19 +0300

    add clamp util function

--- src/utils.ts
+++ src/utils.ts
@@ -13,3 +13,6 @@ export function addIfNoCopies<T extends WithId>(array: T[], item: T) {
   if (array.find((element) => element.id === item.id)) return array;
   return [...array, item];
 }
+
+export const clamp = (value: number, min: number, max: number) =>
+  Math.min(max, Math.max(min, value));
